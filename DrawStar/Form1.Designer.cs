a9f7da83ac8c788df621908102a27bb18ee78cd7
reynen71 <tasireyn943@ed.amdsb.ca>
2015-11-26 13:53:06 -0500

    Finished Draw Star Program- Tasia

---
+++ DrawStar/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace DrawStar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawButton = new System.Windows.Forms.Button();
+            this.xInput = new System.Windows.Forms.TextBox();
+            this.yInput = new System.Windows.Forms.TextBox();
+            this.sizeInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(411, 79);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(75, 23);
+            this.drawButton.TabIndex = 0;
+            this.drawButton.Text = "Draw";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // xInput
+            // 
+            this.xInput.Location = new System.Drawing.Point(437, 1);
+            this.xInput.Name = "xInput";
+            this.xInput.Size = new System.Drawing.Size(49, 20);
+            this.xInput.TabIndex = 1;
+            // 
+            // yInput
+            // 
+            this.yInput.Location = new System.Drawing.Point(437, 27);
+            this.yInput.Name = "yInput";
+            this.yInput.Size = new System.Drawing.Size(49, 20);
+            this.yInput.TabIndex = 2;
+            // 
+            // sizeInput
+            // 
+            this.sizeInput.Location = new System.Drawing.Point(437, 53);
+            this.sizeInput.Name = "sizeInput";
+            this.sizeInput.Size = new System.Drawing.Size(49, 20);
+            this.sizeInput.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(498, 394);
+            this.Controls.Add(this.sizeInput);
+            this.Controls.Add(this.yInput);
+            this.Controls.Add(this.xInput);
+            this.Controls.Add(this.drawButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.TextBox xInput;
+        private System.Windows.Forms.TextBox yInput;
+        private System.Windows.Forms.TextBox sizeInput;
+    }
+}
+
---
+++ DrawStar/Form1.cs
@@ -0,0 +1,59 @@
+﻿//Created by: Tasia Reynen
+//Novemember 26th, 2015
+//Draw Star at differnt locations and different sizes of the star
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawStar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DrawStar(20,20,100);
+        }
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            float x = Convert.ToSingle(xInput.Text);       // holds the x value inputed by user
+            float y = Convert.ToSingle(yInput.Text);       // holds the y value inputed by user
+            float size = Convert.ToSingle(sizeInput.Text);      // holds the size value inputed by user
+
+            DrawStar(x, y, size);
+        }
+        /// <summary>
+        /// Draw Star program- calculates a star at a different position and chnages the size
+        /// </summary>
+        /// <param name="x">x positions of the star</param>
+        /// <param name="y">y positions of the star</param>
+        /// <param name="size">size of star</param>
+        private void DrawStar(float x, float y, float size)
+        {
+            Graphics formGraphics = this.CreateGraphics();
+            Pen blackPen = new Pen(Color.White);
+
+            // Moving the star & changing size
+            formGraphics.Clear(Color.Black);
+            formGraphics.DrawLine(blackPen, (0 + x) *(size / 205), (76 + y)*(size / 197), (78 + x) * (size / 205), (76 + y) * (size / 197));
+            formGraphics.DrawLine(blackPen, (78 + x) * (size / 205), (76 + y) * (size / 197), (103 + x) * (size / 205), (0 + y) * (size / 197));
+            formGraphics.DrawLine(blackPen, (103 + x)* (size / 205), (0 + y) * (size / 197), (127 + x) * (size / 205), (76 + y) * (size / 197));
+            formGraphics.DrawLine(blackPen, (127 + x) * (size / 205), (76 + y) * (size / 197), (205 + x) * (size / 205), (76 + y) * (size / 197));
+            formGraphics.DrawLine(blackPen, (205 + x) * (size / 205), (76 + y) * (size / 197), (142 + x) * (size / 205), (123 + y) * (size / 197));
+            formGraphics.DrawLine(blackPen, (142 + x) * (size / 205), (123 + y) * (size / 197), (165 + x) * (size / 205), (197 + y) * (size / 197));
+            formGraphics.DrawLine(blackPen, (165 + x) * (size / 205), (197 + y) * (size / 197), (103 + x) * (size / 205), (151 + y) * (size / 197));
+            formGraphics.DrawLine(blackPen, (103 + x) * (size / 205), (151 + y) * (size / 197), (40 + x) * (size / 205), (197 + y) * (size / 197));
+            formGraphics.DrawLine(blackPen, (40 + x) * (size / 205), (197 + y) * (size / 197), (64 + x) * (size / 205), (123 + y) * (size / 197));
+            formGraphics.DrawLine(blackPen, (64 + x) * (size / 205), (123 + y) * (size / 197), (0 + x) * (size / 205), (76 + y) * (size / 197));
+        }
+    }
+
+   
+}
